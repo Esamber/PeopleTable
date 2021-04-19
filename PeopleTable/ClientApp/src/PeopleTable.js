a0875911a28bdb84f78c12de46ea7a6ab7ecb66f
Elka Samber <ersamber@gmail.com>
2021-04-18 23:01:50 -0400

    Add project files.

---
+++ PeopleTable/ClientApp/src/PeopleTable.js
@@ -0,0 +1,69 @@
+﻿import { extend } from 'jquery';
+import React from 'react';
+import PersonRow from './PersonRow';
+import PersonForm from './PersonForm'
+
+class PeopleTable extends React.Component {
+
+    state = {
+        person: { firstName: '', lastName: '', age: '' },
+        people: []
+    }
+
+    onFirstNameChange = e => {
+        this.setState({ person: { ...this.state.person, firstName: e.target.value } });
+    }
+    onLastNameChange = e => {
+        this.setState({ person: { ...this.state.person, lastName: e.target.value } });
+        console.log(this.person)
+    }
+    onAgeChange = e => {
+        this.setState({ person: { ...this.state.person, age: e.target.value } });
+    }
+    onAddClick = () => {
+        console.log(this.people)
+        const { person, people } = this.state;
+        const peoplecopy = [...people, person];
+        this.setState({ people: peoplecopy, person: { firstName: '', lastName: '', age: '' } });
+    }
+    onClearClick = () => {
+        this.setState({ people: [] });
+    }
+
+    generateTable = () => {
+        if (this.state.people.length == 0) {
+            return < h2> No people yet - be the first to add!</h2 >
+        }
+        else {
+            return (
+            <table className="table table-hover table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.people.map((p, idx) => <PersonRow person={p} key={idx} />)}
+                </tbody>
+            </table>
+        )}
+    }
+
+    render() {
+        return (
+            <div className="container mt-5">
+                <PersonForm
+                    onFirstNameChange={this.onFirstNameChange}
+                    onLastNameChange={this.onLastNameChange}
+                    onAgeChange={this.onAgeChange}
+                    onAddClick={this.onAddClick}
+                    onClearClick={this.onClearClick}
+                    person={this.state.person} />
+                {this.generateTable()}
+            </div>
+        );
+    }
+}
+export default PeopleTable;
---
+++ PeopleTable/ClientApp/src/PersonForm.js
@@ -0,0 +1,27 @@
+﻿import React from 'react';
+
+class PersonForm extends React.Component {
+
+    render() {
+        return (
+        <div className="row jumbotron">
+            <div className="col-md-3">
+                <input type="text" className="form-control" value={this.props.person.firstName} onChange={this.props.onFirstNameChange} placeholder="First Name"/>
+            </div>
+            <div className="col-md-3">
+                <input type="text" className="form-control" value={this.props.person.lastName} onChange={this.props.onLastNameChange} placeholder="Last Name"/>
+            </div>
+            <div className="col-md-3">
+                <input type="text" className="form-control" value={this.props.person.age} onChange={this.props.onAgeChange} placeholder="Age"/>
+            </div>
+            <div className="col-md-1">
+                <button className="btn btn-primary" onClick={this.props.onAddClick}>Add</button>
+            </div>
+            <div className="col-md-2">
+                <button className="btn btn-danger" onClick={this.props.onClearClick}>Clear All</button>
+            </div>
+        </div>
+    )}
+
+}
+export default PersonForm;
